44f1ca71fd996238d1587acfa3383ca4eb145282
Angel <angmejia0324@gmail.com>
2023-05-10 00:08:46 -0600

    feat:vu3+quasar

---
+++ src/components/ButtonConter.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-btn color="primary" label="sumar" @click="sumarBien()" />
+</template>
+
+<script setup>
+import { ref } from "vue";
+const conter = ref(0);
+
+const emit = defineEmits(["sumarContador"]);
+
+const sumarBien = () => {
+  conter.value++;
+  emit("sumarContador", conter);
+};
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <button-conter :contador="contador" @sumarContador="sumRes"></button-conter>
+    <h1>{{ contador }}</h1>
+
+    <h1>------------------</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import ButtonConter from "../components/ButtonConter.vue";
+
+const contador = ref(0);
+const active = false;
+const name = "Gabo";
+const fruta = {
+  name: "manzana",
+  price: "$5",
+};
+const colors = ["blue", "red", "green"];
+const arrayFrutas = [
+  {
+    name: "Manzana",
+    price: "$1.00",
+    description: "Una manzana",
+    stock: 0,
+  },
+  {
+    name: "Pera",
+    price: "$2.00",
+    description: "Una pera",
+    stock: 10,
+  },
+  {
+    name: "Naranja",
+    price: "$3.00",
+    description: "Una naranja",
+    stock: 20,
+  },
+];
+
+const sumRes = (conter) => {
+  contador.value = conter;
+};
+</script>
